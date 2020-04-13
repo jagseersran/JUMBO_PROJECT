0d2e8be272396bcfcd1f79caa207003210df6f00
SONY <gauravchopra1168@gmail.com>
2020-04-13 18:46:09 -0400

    Changes in mainactivity part2 (delcare variable)

--- app/src/main/java/com/example/employeemanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/employeemanagementsystem/MainActivity.java
@@ -31,3 +31,29 @@ public class MainActivity extends AppCompatActivity {
     String Vcategory = new String();
     String Vcolor = new String();
     DataHelper myDB ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDB = new DataHelper(this);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Choose type");
+        arrayList.add("Manager");
+        arrayList.add("Tester");
+        arrayList.add("Programmer");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner spinner = findViewById(R.id.spinnerEType);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String tutorialsName = parent.getItemAtPosition(position).toString();
+                final TextView lblBNo = findViewById(R.id.lblBNo);
+                final EditText txtBNo = findViewById(R.id.txtBNo);
+                final TextView lblCNo = findViewById(R.id.lblCNo);
+                final EditText txtCNo = findViewById(R.id.txtCNo);
+                final TextView lblProjNo = findViewById(R.id.lblPNo);
+                final EditText txtProjNo = findViewById(R.id.txtPNo);
+            }
